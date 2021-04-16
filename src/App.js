e15ab852e8d3a427cfbdd7dd7a45dedace8a3083
Dimitrios Akritidis <Jimko25@yahoo.gr>
2021-04-16 09:44:52 +0200

    testing

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import React from 'react';
 import { Link, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Sidebar from './components/Sidebar';
+import Message from './components/Message';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-       <Sidebar />
-       <Switch>
-        <Route>
-          <Route path="/meessages/:id" />
-        </Route>
-      </Switch> 
-  
-      <div className="Main"></div> 
+      {/* <h1>Hello</h1> */}
+
+      </div> 
      
-    </div>
+
   );
 }
 
 export default App;
 
+       {/* <Sidebar />
+       <Switch>
+        <Route>
+          <Route path="/messages/:id" />
+        </Route>
+      </Switch> 
+  
+      <div className="Main">
+        <Message />    </div> */}
---
+++ src/components/Message.js
@@ -0,0 +1,39 @@
+function Message() {
+
+//     const [message, setMessage] = useState([]);
+
+//     useEffect(() => {
+//         const fetchMessage = async () => {
+//             const messages = await 
+//                 .get('http://api.icndb.com/jokes/random')
+//                 .then((result) => result.data);
+//                 .then ((json) => {
+//                   setMessage(json);
+//                 })
+//                 .catch(console.log('request failed'));
+//             return message;
+//         };
+//         fetchMessage().then((res) => setMessage(res));
+//     }, []);
+//     function getMessage(
+//    url = "http://api.icndb.com/jokes/random"
+//    )    {
+//         fetch(url)
+//             .then((res) => res.json())
+//             .then((json) => {
+//                 setMessage(json.value.joke)
+//             })
+//             .catch(() => console.log("Chuck Noris says Blyat"));
+//     }
+
+//         useEffect(getMessage, []);
+
+//         return [message, getMessage]
+    return (
+        <div>
+           
+        </div>
+    )
+}
+
+export default Message;
